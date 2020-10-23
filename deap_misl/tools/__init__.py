78abe935439bb65fef5147542716d69c28e3907c
Junji Sawada <junjis0203@gmail.com>
2020-10-23 13:05:44 +0900

    implement AdaptiveParameterWrapper

--- deap_misl/tools/__init__.py
+++ deap_misl/tools/__init__.py
@@ -2,3 +2,15 @@ from .my_emo import selNSGA2, selTournamentDCD
 from .selNSGA3 import selNSGA3
 
 from .twodim import cxTwoPoint2d, cx2d, mutate2d
+
+class AdaptiveParameterWrapper:
+    def __init__(self, func, arg_name, arg_logic):
+        self.func = func
+        self.arg_name = arg_name
+        self.arg_logic = arg_logic
+        self.call_count = 0
+
+    def __call__(self, *args, **kwargs):
+        self.call_count += 1
+        kwargs[self.arg_name] = self.arg_logic(self.call_count)
+        return self.func(*args, **kwargs)
--- examples/onemax_2d.py
+++ examples/onemax_2d.py
@@ -25,9 +25,19 @@ toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 def evalOneMax(individual):
     return sum([sum(row) for row in individual]),
 
+def adaptive_area_max(count):
+    # 交叉は個体数に対して半分の回数が呼ばれる（2個体ずつ渡されるため）
+    # また、交叉率を掛けることで「現在の世代」を推定できる
+    gen = int(count / ((300 / 2) * 0.5))
+    # 最大面積を50とし、1世代ごとに最大面積を小さくしていく
+    return 50 - gen
+
+adaptive_cx2d = misl_tools.AdaptiveParameterWrapper(misl_tools.cx2d, "area_max", adaptive_area_max)
+
 toolbox.register("evaluate", evalOneMax)
 #toolbox.register("mate", misl_tools.cxTwoPoint2d)
-toolbox.register("mate", misl_tools.cx2d, area_max=50, rounding=round)
+#toolbox.register("mate", misl_tools.cx2d, area_max=50, rounding=round)
+toolbox.register("mate", adaptive_cx2d, rounding=round)
 toolbox.register("mutate1d", tools.mutFlipBit, indpb=0.05)
 toolbox.register("mutate", misl_tools.mutate2d, toolbox=toolbox)
 toolbox.register("select", tools.selTournament, tournsize=3)
