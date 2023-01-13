2cda3060e7644e20673fa7ad0e1b1c9e6493f5bd
Junji Sawada <junjis0203@gmail.com>
2023-01-13 11:39:15 +0900

    add AdaptiveParameterWrapper doc

--- deap_misl/tools/__init__.py
+++ deap_misl/tools/__init__.py
@@ -1,16 +1,5 @@
 from .my_emo import selNSGA2, selTournamentDCD
 from .selNSGA3 import selNSGA3
 
-from .twodim import cxTwoPoint2d, cx2d, cxSimple2dExtend, mutSimple2dExtend
-
-class AdaptiveParameterWrapper:
-    def __init__(self, func, arg_name, arg_logic):
-        self.func = func
-        self.arg_name = arg_name
-        self.arg_logic = arg_logic
-        self.call_count = 0
-
-    def __call__(self, *args, **kwargs):
-        self.call_count += 1
-        kwargs[self.arg_name] = self.arg_logic(self.call_count)
-        return self.func(*args, **kwargs)
+from .utils import *
+from .twodim import *
--- examples/onemax_2d.py
+++ examples/onemax_2d.py
@@ -25,27 +25,31 @@ toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 def evalOneMax(individual):
     return sum([sum(row) for row in individual]),
 
+N = 300
+CXPB = 0.5
+MUTPB = 0.2
+NGEN = 40
+AREA_MAX = 50
+
 def adaptive_area_max(count):
-    # 交叉は個体数に対して半分の回数が呼ばれる（2個体ずつ渡されるため）
-    # また、交叉率を掛けることで「現在の世代」を推定できる
-    gen = int(count / ((300 / 2) * 0.5))
-    # 最大面積を50とし、1世代ごとに最大面積を小さくしていく
-    return 50 - gen
+    # Crossover is called half the number of individuals.
+    # So, we can estimate "current generation" using crossover rate.
+    gen = int(count / ((N / 2) * CXPB))
+    # Decrement area_max by generation.
+    return AREA_MAX - gen
 
 adaptive_cx2d = misl_tools.AdaptiveParameterWrapper(misl_tools.cx2d, "area_max", adaptive_area_max)
 
 toolbox.register("evaluate", evalOneMax)
 #toolbox.register("mate", misl_tools.cxTwoPoint2d)
-#toolbox.register("mate", misl_tools.cx2d, area_max=50, rounding=round)
+#toolbox.register("mate", misl_tools.cx2d, area_max=AREA_MAX, rounding=round)
 toolbox.register("mate", adaptive_cx2d, rounding=round)
 toolbox.register("mutate1d", tools.mutFlipBit, indpb=0.05)
 toolbox.register("mutate", misl_tools.mutSimple2dExtend, mut1d=toolbox.mutate1d)
 toolbox.register("select", tools.selTournament, tournsize=3)
 
 def main():
-    random.seed(64)
-    
-    pop = toolbox.population(n=300)
+    pop = toolbox.population(n=N)
     hof = tools.HallOfFame(1)
     stats = tools.Statistics(lambda ind: ind.fitness.values)
     stats.register("avg", numpy.mean)
@@ -53,10 +57,11 @@ def main():
     stats.register("min", numpy.min)
     stats.register("max", numpy.max)
     
-    pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=40, 
+    pop, log = algorithms.eaSimple(pop, toolbox, cxpb=CXPB, mutpb=MUTPB, ngen=NGEN, 
                                    stats=stats, halloffame=hof, verbose=True)
     
     return pop, log, hof
 
 if __name__ == "__main__":
+    random.seed(64)
     main()
